0bea860b3de31c3f6d95e406dd4cbeda1d51271e
Amraoua Zidani <amra.zidani@gmail.com>
2017-02-05 18:56:31 +0100

    correction template not found

--- LicensesPlates/views.py
+++ LicensesPlates/views.py
@@ -1,12 +1,9 @@
 import json as libjson
 from LicensesPlates import PlateReader
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import RequestContext, loader
-
+from django.template import RequestContext
 
 def home(request):
-    template = loader.get_template('home.html')
     context = RequestContext(request)
     return render(request, 'home.html', context)
 
@@ -15,6 +12,5 @@ def capturePlate(request):
     plate_reader = PlateReader.ReadPlates()
     response, errors = plate_reader.alpr_json_results()
     results = response.get('results')
-    template = loader.get_template('check.html')
     context = RequestContext(request, {'plate': results[0].get('plate'), 'confidence': results[0].get('confidence')})
     return render(request, 'check.html', context)
