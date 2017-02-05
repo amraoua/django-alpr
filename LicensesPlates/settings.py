8b38f4708c444bc4acc4f8040f81d86261ba8c4f
Amraoua Zidani <amra.zidani@gmail.com>
2017-02-05 11:55:08 +0100

    gunicorn install and static files config

--- LicensesPlates/settings.py
+++ LicensesPlates/settings.py
@@ -81,15 +81,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.dirname(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = os.path.dirname(os.path.dirname(BASE_DIR),"django-alpr-static-root")
-
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.dirname(BASE_DIR, "static"),
+# ]
+# STATIC_ROOT = os.path.dirname(os.path.dirname(BASE_DIR),"django-alpr-static-root")
 # STATIC_MEDIA = os.path.dirname(os.path.dirname(BASE_DIR),"django-alpr-media-root")
+
+MEDIA_ROOT = '/var/www/media/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/var/www/static/'
+STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
