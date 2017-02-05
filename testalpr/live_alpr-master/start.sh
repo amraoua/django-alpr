3add3ed3c8fb9e7834dfdb4bf09fcada34a6d5ed
Amraoua Zidani <amra.zidani@gmail.com>
2017-02-05 17:01:11 +0100

    adding a new test live alpr

--- testalpr/live_alpr-master/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /Documents/projects/django-alpr/testalpr/live_alpr-master
-python -u live_alpr.py | tee log
---
+++ testalpr/live_alpr/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /Documents/projects/django-alpr/testalpr/live_alpr-master
+python -u /Documents/projects/django-alpr/testalpr/live_alpr/live_alpr.py | tee log
