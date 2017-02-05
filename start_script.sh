683a7b7c7c4bb694daa8ed935b01fb02ed06604a
Amraoua Zidani <amra.zidani@gmail.com>
2017-02-05 12:34:27 +0100

    added "gunicorn.sh"

--- start_script.sh
+++ start_script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-pushd /home/michal/app
-source /home/pi/venv/venv1/bin/activate
+WORKING_DIR= /home/pi/Documents/projects/django-alpr
+ACTIVATE_PATH= /home/pi/venv/venv1/bin/activate
+cd ${WORKING_DIR}
+source ${ACTIVATE_PATH}
+exec $@
