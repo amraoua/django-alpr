85e26a58b72cbf9263207585febf6891ce0c16a4
Amraoua Zidani <amra.zidani@gmail.com>
2017-02-05 18:04:32 +0100

    correction template not found

--- LicensesPlates/settings.py
+++ LicensesPlates/settings.py
@@ -100,7 +100,15 @@ MEDIA_URL = '/media/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
-        'DIRS': os.path.join(BASE_DIR, 'templates'),
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
